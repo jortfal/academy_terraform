5ed147a395472429060cc57678156aa2425ba6a6
jortfal <jortfal@users.noreply.github.com>
2020-10-18 12:43:36 +0200

    feat: add global variables

---
+++ ejemplos/azure/variables.tf
@@ -0,0 +1,11 @@
+/* 
+Copyright 2020 José Mnauel Ortega Falcón
+*/
+
+# Global Variables 
+
+variable "default_location" {
+    description = "The location where the Azure Resources should be created"
+    type        = string
+    default     = "West Europe"
+}
